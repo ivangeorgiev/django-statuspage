8b7ba7fade32367348d861e1c9ac343d0b5c12eb
Ivan Georgiev <ivan.georgiev@gmail.com>
2021-12-15 19:00:07 +0200

    missing environment variable fails docs build

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,8 @@ import sys
 import django
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../src'))
+os.environ['DJANGO_SECRET_KEY'] = 'Not-so-secret'
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'djangostatuspage.tests.mystatuspage.settings'
 django.setup()
 
