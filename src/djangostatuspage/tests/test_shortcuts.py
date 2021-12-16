83da38e56054519eabb4820ba661aa140aa50c15
Ivan Georgiev <ivan.georgiev@gmail.com>
2021-12-16 18:58:37 +0200

    pre-commit in ci pipeline

--- src/djangostatuspage/tests/test_shortcuts.py
+++ src/djangostatuspage/tests/test_shortcuts.py
@@ -12,53 +12,48 @@ pytestmark = [pytest.mark.unit]
 
 class TestCamelCaseSplit:
     """Test cases for camel_case_split function"""
-
-    @pytest.mark.parametrize("input,expected",
-                             [
-                                 ('PascalCase', ['Pascal', 'Case']),
-                                 ('camelCase', ['camel', 'Case']),
-                                 ('Ucfirst', ['Ucfirst']),
-                                 ('lowercase', ['lowercase']),
-                                 ('', ['']),
-                                 ('ABBR', ['A', 'B', 'B', 'R']),
-                             ])
-    def test_called_with(self, input, expected):
-        result = shortcuts.camel_case_split(input)
+    @pytest.mark.parametrize("data,expected", [
+        ('PascalCase', ['Pascal', 'Case']),
+        ('camelCase', ['camel', 'Case']),
+        ('Ucfirst', ['Ucfirst']),
+        ('lowercase', ['lowercase']),
+        ('', ['']),
+        ('ABBR', ['A', 'B', 'B', 'R']),
+    ])
+    def test_called_with(self, data, expected):
+        result = shortcuts.camel_case_split(data)
         assert result == expected
 
 
 class TestGetEnumChoices:
     """Test cases for get_enum_choices function"""
-
     class MyEnum(enum.Enum):
         OPTION_1 = 1
         OPTION_2 = 2
 
     def test_called(self):
-        input = self.MyEnum
+        data = self.MyEnum
         expect = [(1, 'Option 1'), (2, 'Option 2')]
-        result = shortcuts.get_enum_choices(input)
+        result = shortcuts.get_enum_choices(data)
         assert result == expect
 
 
 class TestMultiCaseSplit:
     """Test cases for multi_case_split function"""
-
-    @pytest.mark.parametrize("input,expected",
-                             [
-                                 ('PascalCase', ['Pascal', 'Case']),
-                                 ('camelCase', ['camel', 'Case']),
-                                 ('Ucfirst', ['Ucfirst']),
-                                 ('snake_case', ['snake', 'case']),
-                                 ('kebap-case', ['kebap', 'case']),
-                                 ('lowercase', ['lowercase']),
-                                 ('mixed-PacalCase_snake_case-camelCase', ['mixed', 'Pacal', 'Case',
-                                                                           'snake', 'case', 'camel', 'Case']),
-                                 ('', ['']),
-                                 ('-stripped-empty---Between-', ['stripped', 'empty', 'Between']),
-                             ])
-    def test_called_with(self, input, expected):
-        result = shortcuts.multi_case_split(input)
+    @pytest.mark.parametrize("data,expected", [
+        ('PascalCase', ['Pascal', 'Case']),
+        ('camelCase', ['camel', 'Case']),
+        ('Ucfirst', ['Ucfirst']),
+        ('snake_case', ['snake', 'case']),
+        ('kebap-case', ['kebap', 'case']),
+        ('lowercase', ['lowercase']),
+        ('mixed-PacalCase_snake_case-camelCase',
+         ['mixed', 'Pacal', 'Case', 'snake', 'case', 'camel', 'Case']),
+        ('', ['']),
+        ('-stripped-empty---Between-', ['stripped', 'empty', 'Between']),
+    ])
+    def test_called_with(self, data, expected):
+        result = shortcuts.multi_case_split(data)
         assert result == expected
 
     def test_name(self):
@@ -68,19 +63,16 @@ class TestMultiCaseSplit:
 
 class TestMakeVerboseName:
     """Test cases for the make_verbose_name function"""
-
-    @pytest.mark.parametrize("input,expect",
-                             [
-                                 ('the-Name_here', 'The name here'),
-                             ])
-    def test_called(self, input, expect):
-        result = shortcuts.make_verbose_name(input)
+    @pytest.mark.parametrize("data,expect", [
+        ('the-Name_here', 'The name here'),
+    ])
+    def test_called(self, data, expect):
+        result = shortcuts.make_verbose_name(data)
         assert result == expect
 
 
 class TestUtcNow(unittest.TestCase):
     """Test cases for the utc_now function"""
-
     @mock.patch('datetime.datetime')
     def test_returns_result_from_datetime_replace(self, dt_mock):
         utcnow_mock = mock.Mock()
